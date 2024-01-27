63d3b0b528f58c06bb9b14faf439d3af9b9ef6f4
nyoongoon <nyoongoon@naver.com>
2024-01-27 22:15:30 +0900

    단위테스트를 위한 jpa 설정 분리

--- api/src/main/kotlin/com/example/api/ApiApplication.kt
+++ api/src/main/kotlin/com/example/api/ApiApplication.kt
@@ -6,9 +6,8 @@ import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-@EnableJpaAuditing
+
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = ["com.example.domain"])
 @EntityScan(basePackages = ["com.example.domain"])
 class ApiApplication
 
