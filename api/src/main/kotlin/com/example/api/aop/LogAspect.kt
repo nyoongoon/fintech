8922156ebb6def385f77adec43a4d93069f019bf
nyoongoon <nyoongoon@naver.com>
2024-01-27 23:06:53 +0900

    aop 설정으로 로깅 추가

---
+++ api/src/main/kotlin/com/example/api/aop/LogAspect.kt
@@ -0,0 +1,25 @@
+package com.example.api.aop
+
+import mu.KotlinLogging
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
+import org.springframework.stereotype.Component
+
+@Aspect
+@Component
+class LogAspect {
+    val logger = KotlinLogging.logger {}
+
+    // 1. 조인포인트 가져오는 부분
+    @Pointcut("within(com.example.api..*)") // 특정 시점을 가져오는 것 -> within: 패키지 내의 메서드들이 실행되는 시점으로 설정
+    private fun isApi() {
+    }
+
+    // 2. 조인포인트를 가져왔을 때 어떤 부분을 할 건지
+    @Around("isApi()")
+    fun loggingAspect(joinPoint: ProceedingJoinPoint): Any {
+
+    }
+}
