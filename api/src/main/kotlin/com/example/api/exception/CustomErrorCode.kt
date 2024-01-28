cae6b3eaa0f4a341d8cc8d87f5d2c196626faf25
nyoongoon <nyoongoon@naver.com>
2024-01-29 06:15:23 +0900

    exception 처리 -> 1. ControllerAdvice 2. ExceptionHandler 추가 중

---
+++ api/src/main/kotlin/com/example/api/exception/CustomErrorCode.kt
@@ -0,0 +1,4 @@
+package com.example.api.exception
+
+class CustomErrorCode {
+}
---
+++ api/src/main/kotlin/com/example/api/loan/review/LoanReviewControllerAdvice.kt
@@ -0,0 +1,4 @@
+package com.example.api.loan.review
+
+class LoanReviewControllerAdvice { // exception 처리 -> 1. ControllerAdvice 2. ExceptionHandler
+}
