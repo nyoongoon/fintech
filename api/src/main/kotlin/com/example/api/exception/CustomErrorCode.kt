70db2e0fe23fe753b0b5072c04ed06e6bebf10d6
nyoongoon <nyoongoon@naver.com>
2024-01-30 06:10:23 +0900

    에러 관련 클래스 생성 및 nullable 작업 추가

--- api/src/main/kotlin/com/example/api/exception/CustomErrorCode.kt
+++ api/src/main/kotlin/com/example/api/exception/CustomErrorCode.kt
@@ -1,4 +1,11 @@
 package com.example.api.exception
 
-class CustomErrorCode {
+import org.springframework.http.HttpStatus
+
+enum class CustomErrorCode (
+    val statusCode: HttpStatus,
+    val errorCode: String,
+    val errorMessage: String
+){
+    RESULT_NOT_FOUND(HttpStatus.BAD_REQUEST, "E001", errorMessage = "result not found")
 }
---
+++ api/src/main/kotlin/com/example/api/exception/CustomException.kt
@@ -0,0 +1,4 @@
+package com.example.api.exception
+
+class CustomException(val customErrorCode: CustomErrorCode): RuntimeException() {
+}
