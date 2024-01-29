cdac007b0a8eebe23dafb38b98d4adfd9a4d706a
nyoongoon <nyoongoon@naver.com>
2024-01-30 06:25:46 +0900

    컨트롤러 어드바이스 및 익셉션 핸들러 생성

--- api/src/main/kotlin/com/example/api/loan/review/LoanReviewControllerAdvice.kt
+++ api/src/main/kotlin/com/example/api/loan/review/LoanReviewControllerAdvice.kt
@@ -1,4 +1,15 @@
 package com.example.api.loan.review
 
+import com.example.api.exception.CustomException
+import com.example.api.exception.ErrorResponse
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice(basePackageClasses = [LoanReviewController::class])
 class LoanReviewControllerAdvice { // exception 처리 -> 1. ControllerAdvice 2. ExceptionHandler
+    // 특정 에러코드별로 처리할 때 @ExceptionHandler 사용..
+    @ExceptionHandler(CustomException::class)
+    fun customExceptionHandler(customException: CustomException) =
+        ErrorResponse(customException).toResponseEntity()
+
 }
--- api/src/main/kotlin/com/example/api/loan/review/LoanReviewServiceImpl.kt
+++ api/src/main/kotlin/com/example/api/loan/review/LoanReviewServiceImpl.kt
@@ -15,7 +15,7 @@ class LoanReviewServiceImpl(
         return LoanReviewDto.LoanReviewResponseDto(
             userKey = userKey,
             loanResult = getLoanResult(userKey)?.toResponseDto()
-                ?: throw CustomException(CustomErrorCode.RESULT_NOT_FOUND)
+                ?: throw CustomException(CustomErrorCode.RESULT_NOT_FOUND) // nullable
         )
     }
 
